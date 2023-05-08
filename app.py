52266b8ad678f7afa1ba2cd335e7bf4c30697b6b
Piotr Ferenc <ferenc.piotr@outlook.com>
2023-05-08 15:38:03 +0200

    Fix app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Zadanie: test-github-workflow
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Return a message with a header.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 
